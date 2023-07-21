deaa0bfa2bed70fa93158fa4e5084430d9df6260
Axe Botz <136898915+Axe-Botz@users.noreply.github.com>
2023-07-21 10:50:48 +0530

    Update x.py

--- x.py
+++ x.py
@@ -21,3 +21,12 @@ async def start(client: Client, message: Message):
 
 
 @Client.on_callback_query(filters.regex("zeno"))
+async def queued_tracks(client, CallbackQuery: CallbackQuery, _):
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    await CallbackQuery.answer()
+    buttons =
+    med = InputMediaPhoto(
+        media="https://telegra.ph//file/6f7d35131f69951c74ee5.jpg"        
+    )
+    await CallbackQuery.edit_message_media(media=med)
